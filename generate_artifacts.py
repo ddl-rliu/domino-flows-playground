201153af025ff6cd4f80e1fa76ec3e42fa758ae2
ddl-rliu <richard.liu@dominodatalab.com>
2026-06-25 13:01:55 -0700

    Add workflow generate_artifacts_and_use_path_config

--- generate_artifacts.py
+++ generate_artifacts.py
@@ -1,9 +1,10 @@
 from flytekit import workflow
 from flytekit.types.file import FlyteFile
 from flytekit.types.directory import FlyteDirectory
+from flytekitplugins.domino.file import PathConfig
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef
 from flytekitplugins.domino.artifact import Artifact, DATA, MODEL, REPORT
-from typing import TypeVar, Optional, List, Dict
+from typing import Annotated, TypeVar, Optional, List, Dict
 import pandas as pd
 
 ReportArtifact = Artifact(name="My Report", type=REPORT)
@@ -115,3 +116,49 @@ def generate_artifacts_with_netapp_volume_exports():
     return 
 
 
+# pyflyte run --remote generate_artifacts.py generate_artifacts_and_use_path_config
+@workflow
+def generate_artifacts_and_use_path_config(): 
+
+    sce_types = DominoJobTask(
+        name="Generate SCE Types",
+        domino_job_config=DominoJobConfig(MainRepoGitRef=GitRef(Type="head"),
+                                          Command="python /mnt/code/scripts/generate-sce-types.py"),
+        inputs={'sdtm_data_path': str},
+        outputs={'pdf': ReportArtifact.File(name="generated.pdf"), 'sas7bdat': DataArtifact.File(name="data.sas7bdat")},
+        use_latest=True
+    )
+
+    sce_types(sdtm_data_path="/mnt/code/artifacts")
+
+    ml_types = DominoJobTask(
+        name="Generate ML Types",
+        domino_job_config=DominoJobConfig(MainRepoGitRef=GitRef(Type="head"),
+                                          Command="env OUTPUT_DIR=/mnt/data/flow-outputs SHOULD_APPEND_FILE_EXT=true python /mnt/code/scripts/generate-ml-types.py"),
+        inputs={
+            'batch_size': int,
+            'learning_rate': float,
+            'do_eval': bool,
+            'list': List[int],
+            'dict': Dict[str,int]
+        },
+        outputs={
+            'csv': Annotated[DataArtifact.File(name="data.csv"), PathConfig(path="/mnt/data/flow-outputs/data.csv")],
+            'json': Annotated[DataArtifact.File(name="data.json"), PathConfig(path="/mnt/data/flow-outputs/test.json")],
+            'png': Annotated[ReportArtifact.File(name="report.png"), PathConfig(path="/mnt/data/flow-outputs/plot.png")],
+            'jpeg': Annotated[ReportArtifact.File(name="report.jpeg"), PathConfig(path="/mnt/data/flow-outputs/plot.jpeg")],
+            'notebook': Annotated[DataArtifact.File(name="notebook.ipynb"), PathConfig(path="/mnt/data/flow-outputs/notebook.ipynb")],
+            'pkl': Annotated[ModelArtifact.File(name="intro.pkl"), PathConfig(path="/mnt/data/flow-outputs/intro.pkl")],
+        },
+        use_latest=True
+
+    )
+
+    ml_types(batch_size=32,
+            learning_rate=0.001,
+            do_eval=True,
+            list=[1,2,3,4,5],
+            dict={"param1": 1, "param2": 2,"param3": 3}
+            )
+
+    return
